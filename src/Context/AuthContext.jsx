125d2a1cfc132b60bb6a3e0080b59ad7f23e70d3
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2021-06-07 00:54:26 +0530

    Added v2 support

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,15 +1,27 @@
 import { createContext, useContext, useEffect, useReducer } from 'react';
+import { useNavigate } from 'react-router';
 import { initialAuthState, authDispatchFunction } from '../Reducer/authReducer';
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
 	const [authState, authDispatch] = useReducer(authDispatchFunction, initialAuthState);
+	const navigate = useNavigate();
+
+	function previousVersionUserCleanup() {
+		localStorage.removeItem('Login');
+		alert('This is Dove v2. Signup to continue.');
+		navigate('/signup');
+	}
 
 	useEffect(() => {
 		const memory = JSON.parse(localStorage.getItem('Login'));
 		if (memory?.isUserLoggedIn === true) {
-			authDispatch({ type: 'LOGIN_BY_LOCAL_STORAGE', payload: { user: memory } });
+			if (memory.token) {
+				authDispatch({ type: 'LOGIN_BY_LOCAL_STORAGE', payload: { user: memory } });
+			} else {
+				previousVersionUserCleanup();
+			}
 		}
 	}, [authDispatch]);
 
