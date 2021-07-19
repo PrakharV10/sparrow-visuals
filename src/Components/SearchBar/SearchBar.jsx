e5a4b33760c03495330dc54f3b67fe95dbd43fb1
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2021-07-19 18:42:12 +0530

    Fix : Search Feature, Add : Guest Credentials

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -3,23 +3,23 @@ import { useVideo } from '../../Context/VideoContext';
 import './SearchBar.css';
 
 function SearchBar({ searchValue, setSearchValue }) {
-	const { dispatch } = useVideo();
+	const { videoDispatch } = useVideo();
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		dispatch({ type: 'SEARCH_FILTER', payload: searchValue });
+		videoDispatch({ type: 'SEARCH_FILTER', payload: searchValue });
 	}
 
 	return (
-		<form className="search-bar" onSubmit={(e) => handleSubmit(e)}>
+		<form className='search-bar' onSubmit={(e) => handleSubmit(e)}>
 			<input
 				value={searchValue}
 				onChange={(e) => setSearchValue(e.target.value)}
-				placeholder="Search Videos Here"
+				placeholder='Search Videos Here'
 			/>
-			<button type="submit">
-				<svg width="1em" height="1em" viewBox="0 0 32 32">
-					<path d="M29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29zM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9z"></path>
+			<button type='submit'>
+				<svg width='1em' height='1em' viewBox='0 0 32 32'>
+					<path d='M29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29zM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9z'></path>
 				</svg>
 			</button>
 		</form>
--- src/Pages/Login/LogInPage.jsx
+++ src/Pages/Login/LogInPage.jsx
@@ -49,19 +49,19 @@ function LogInPage() {
 	}, [isUserLoggedIn]);
 
 	return (
-		<div className="login-bg image-overlay">
-			<div className="mid-container">
-				<div className="title">
+		<div className='login-bg image-overlay'>
+			<div className='mid-container'>
+				<div className='title'>
 					LOG <span>IN</span>
 				</div>
 
-				<form onSubmit={(e) => submitHandler(e)} className="user-field">
+				<form onSubmit={(e) => submitHandler(e)} className='user-field'>
 					{errorMessage && (
-						<div className="alert error">
-							<svg width="1em" height="1em" viewBox="0 0 24 24">
+						<div className='alert error'>
+							<svg width='1em' height='1em' viewBox='0 0 24 24'>
 								<path
-									d="M13 13h-2V7h2m0 10h-2v-2h2M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2z"
-									fill="currentColor"
+									d='M13 13h-2V7h2m0 10h-2v-2h2M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2z'
+									fill='currentColor'
 								></path>
 							</svg>
 							{errorMessage}
@@ -70,29 +70,36 @@ function LogInPage() {
 					<input
 						onChange={(e) => setLocalInput({ ...localInput, email: e.target.value })}
 						value={localInput.email}
-						placeholder="Email"
-						type="text"
-						name="username"
+						placeholder='Email'
+						type='text'
+						name='username'
 					/>
 					<input
 						onChange={(e) => setLocalInput({ ...localInput, password: e.target.value })}
 						value={localInput.password}
-						placeholder="Password"
-						type="password"
-						name="password"
+						placeholder='Password'
+						type='password'
+						name='password'
 					/>
 
-					<button type="submit" className="btn btn-outline-pink">
+					<button type='submit' className='btn btn-outline-pink'>
 						{loading ? 'LOGGIN IN...' : 'LOG IN'}
 					</button>
 				</form>
-				<div className="sub-text">
+				<div className='sub-text'>
 					Don't have an account?
-					<Link to="/signup">
-						<span className="text-pink"> Sign up!</span>
+					<Link to='/signup'>
+						<span className='text-pink'> Sign up!</span>
 					</Link>
 				</div>
-				<div className="text-pink">Forgot Password?</div>
+				<div
+					onClick={() =>
+						setLocalInput({ email: 'johndoe@gmail.com', password: '123456' })
+					}
+					className='text-pink'
+				>
+					Use Guest Credentials
+				</div>
 			</div>
 		</div>
 	);
